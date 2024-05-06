f93128106d82b9c98166349027e91b5967bea4a5
hasnakharbouche <hasnakharbouchepro@gmail.com>
2024-05-06 14:28:37 +0200

    First commit

---
+++ popup.js
@@ -0,0 +1,39 @@
+// document.getElementById("pageDevoirs").addEventListener("click", function () {
+// chrome.tabs.create({ url: "https://www.kiddle.co" });
+// });
+
+// document.getElementById("pageLecture").addEventListener("click", function () {
+//   chrome.tabs.create({ url: "https://www.kiddle.co" });
+// });
+
+// document.getElementById("pageJeux").addEventListener("click", function () {
+//   chrome.tabs.create({ url: "https://www.kiddle.co" });
+// });
+
+// document.getElementById("youtube").addEventListener("click", function () {
+//   chrome.tabs.create({ url: "https://www.youtubekids.com/" });
+// });
+
+// document.getElementById("cinema").addEventListener("click", function () {
+//   chrome.tabs.create({ url: "https://www.films-pour-enfants.com/index.html" });
+// });
+
+// const mousemove = document.querySelector(".mousemove");
+// window.addEventListener("mousemove", (e)=>{
+//   //console.log(e); // e correspond à la data à tous les évènements = e et e.target montre ou se trouve la souris//
+
+//   mousemove.style.left= e.pageX + "px";
+//   mousemove.style.top=e.pageY + "px";
+// })
+
+// const alerter = () => {
+//   alert("Attention il ne te reste que 5 min pour finir ce que tu fais !");
+// };
+
+// setInterval(alerter, 2000);
+
+setTimeout(() => {
+  alert("Le temps passe vite! Il ne te reste que 5 minutes!");
+}, 2000);
+
+// (3600*1000)-(300*1000)
