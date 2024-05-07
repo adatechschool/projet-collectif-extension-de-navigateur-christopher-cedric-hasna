078e0b78baf5e2137873a0c9a672028214f88cf6
Cédric <atorionebula@gmail.com>
2024-05-07 14:12:09 +0200

    new changepment

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 // document.getElementById("pageDevoirs").addEventListener("click", function () {
-// chrome.tabs.create({ url: "https://www.kiddle.co" });
-// });
+  // chrome.tabs.create({ url: "https://www.kiddle.co" });
+  // }); 
 
 // document.getElementById("pageLecture").addEventListener("click", function () {
 //   chrome.tabs.create({ url: "https://www.kiddle.co" });
@@ -18,13 +18,13 @@
 //   chrome.tabs.create({ url: "https://www.films-pour-enfants.com/index.html" });
 // });
 
-// const mousemove = document.querySelector(".mousemove");
-// window.addEventListener("mousemove", (e)=>{
-//   //console.log(e); // e correspond à la data à tous les évènements = e et e.target montre ou se trouve la souris//
+ /*const mousemove = document.querySelector(".mousemove");
+ window.addEventListener("mousemove", (e)=>{
+    console.log(e); // e correspond à la data à tous les évènements = e et e.target montre ou se trouve la souris//
 
-//   mousemove.style.left= e.pageX + "px";
-//   mousemove.style.top=e.pageY + "px";
-// })
+   mousemove.style.left= e.pageX + "px";
+   mousemove.style.top=e.pageY + "px";
+ })*/
 
 // const alerter = () => {
 //   alert("Attention il ne te reste que 5 min pour finir ce que tu fais !");
